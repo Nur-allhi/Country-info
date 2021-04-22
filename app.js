24341514b18eccfaf9a52b67ea5e0b283d7efadd
Nur-allhi <noorefty1@gmail.com>
2021-04-22 17:57:34 +0600

    Done with the project

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.getElementById("more-info-btn").addEventListener("click", () => {
   document.getElementById("more-info").style.display = "block";
+  document.getElementById("more-info-btn").style.display = "none";
 });
 
 // function moreInfoBtn() {
@@ -17,7 +18,7 @@ const getCountryName = (country) => {
   const url = `${apiBase}${country}?fultext=true`;
   fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => updateData(data));
 };
 
 const sarchBtn = document
@@ -26,3 +27,17 @@ const sarchBtn = document
     const inputCountry = document.getElementById("country-input").value;
     getCountryName(inputCountry);
   });
+
+const updateData = (data) => {
+  document.getElementById("country-name").innerText =
+    data[0].name || "Unknown Location!";
+  document.getElementById("capital").innerText = data[0].capital;
+  document.getElementById("population").innerText = data[0].population;
+  document.getElementById("pass-name").innerText = data[0].altSpellings[1];
+  document.getElementById("region").innerText = data[0].region;
+  document.getElementById("language").innerText = data[0].languages[0].name;
+  document.getElementById("area").innerText = data[0].area;
+  document.getElementById("timezone").innerText = data[0].timezones;
+  document.getElementById("currencie").innerText = data[0].currencies[0].code;
+  document.getElementById("country-input").value = "";
+};
